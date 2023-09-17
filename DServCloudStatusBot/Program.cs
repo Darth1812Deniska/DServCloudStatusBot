e895705d257ff3c4d05f96c6fe2d596bdfcd006b
Denis Davydov <den93mos@yandex.ru>
2023-09-17 21:59:54 +0300

    Проверка подгрузки параметров

--- DServCloudStatusBot/Program.cs
+++ DServCloudStatusBot/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+
+var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json")
+    .Build();
+
+var settings = configuration.GetSection("settings");
+string botToken = settings["bot_token"] ?? string.Empty;
+if (string.IsNullOrEmpty(botToken))
+{
+    return;
+}
+Console.WriteLine(botToken);
