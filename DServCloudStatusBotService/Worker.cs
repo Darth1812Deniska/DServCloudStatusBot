5e79a9573e05d1ec0535aadca04cdb93a82bca11
Denis Davydov <den93mos@yandex.ru>
2023-10-17 13:59:50 +0300

    Обновленная обработка ошибок. Убраны лишние юзинги

--- DServCloudStatusBotService/Worker.cs
+++ DServCloudStatusBotService/Worker.cs
@@ -1,10 +1,13 @@
+using System.Timers;
 using DServCloudStatusCommonClasses;
+using NLog;
+using Timer = System.Timers.Timer;
 
 namespace DServCloudStatusBotService
 {
     public class Worker : BackgroundService
     {
-        private readonly ILogger<Worker> _logger;
+        private Logger Logger => LogManager.GetCurrentClassLogger();
         private BotSettings BotSettings => _botSettings;
         private readonly BotSettings _botSettings;
         private TelegramStatusChecker _telegramStatusChecker;
@@ -12,30 +15,60 @@ namespace DServCloudStatusBotService
 
         public Worker(ILogger<Worker> logger, BotSettings botSettings)
         {
-            _logger = logger;
             this._botSettings = botSettings;
             _telegramStatusChecker = new TelegramStatusChecker(BotSettings);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            Timer timer = new Timer(2000);
+            timer.Elapsed += ExecuteTimer_Elapsed;
+        }
+
+        private void ExecuteTimer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+        {
             
         }
-        
+
         public override async Task StartAsync(CancellationToken cancellationToken)
         {
-            await TelegramStatusChecker.SendMessageToAdminAsync("Сервис запущен");
+            Logger.Info($"StartAsync. Запуск сервиса");
+            
+            Timer startTimer = new Timer(3000);
+            startTimer.Elapsed += StartTimerOnElapsed;
+            startTimer.Start();
         }
-        
+
+        private void StartTimerOnElapsed(object? sender, ElapsedEventArgs e)
+        {
+            Logger.Info($"StartTimerOnElapsed. Итерация таймера {e.SignalTime}");
+            bool isPing = StaticMethods.PingHost("api.telegram.org", 443);
+            if (isPing) 
+            {
+                try
+                {
+                    TelegramStatusChecker.SendMessageToAdminAsync("Сервис запущен");
+                }
+                catch (Exception exception)
+                {
+                    Logger.Error($"StartAsync. {exception.Message}; {exception}");
+                }
+
+                var timer = sender as Timer;
+                timer.Stop();
+            }
+        }
+
         public override async Task StopAsync(CancellationToken cancellationToken)
         {
-            await TelegramStatusChecker.SendMessageToAdminAsync("Сервис останавливается");
+            try
+            {
+                await TelegramStatusChecker.SendMessageToAdminAsync("Сервис останавливается");
+            }
+            catch (Exception e)
+            {
+                Logger.Error($"StopAsync. {e.Message}; {e}");
+            }
         }
-        /*
-        public override Task StartAsync(CancellationToken cancellationToken)
-        {
-            return base.StartAsync(cancellationToken);
-        }
-        */
     }
 }
